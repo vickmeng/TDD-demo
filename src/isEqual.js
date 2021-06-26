b43eac13dc249e70bf62d00dd9ef7450fc1d8fb8
mengweikang <mengweikang@yzw.cn>
2021-06-26 23:36:42 +0800

    【绿色】抽出 isEqualObject

--- src/isEqual.js
+++ src/isEqual.js
@@ -3,24 +3,35 @@ function getType(value) {
 }
 
 /**
- * 继续重构
- * 用switch 处理单输入 多case的情况
+ * 提交QA
  */
+
+/**
+ * 继续重构
+ * 抽出 isEqualObject
+ */
+function isEqualObject(value ,other) {
+  // 略加考虑，保留这两行重复代码不进行抽象，提高阅读流畅性
+  if (value === other) return true
+
+  if (getType(value) !== getType(other)) return false;
+
+  switch (getType(value)) {
+    case '[object Array]':
+    case '[object Object]':
+      if (Object.keys(value).length !== Object.keys(other).length) return false
+      return Object.keys(value).every( key => isEqual(value[key], other[key]))
+    default:
+      return false
+  }
+}
+
 function isEqual(value ,other) {
   if (value === other) return true
 
   if (getType(value) !== getType(other)) return false;
 
-  if (typeof value === 'object') {
-    switch (getType(value)) {
-      case '[object Array]':
-      case '[object Object]':
-        if (Object.keys(value).length !== Object.keys(other).length) return false
-        return Object.keys(value).every( key => isEqual(value[key], other[key]))
-      default:
-        return false
-    }
-  }
+  if (typeof value === 'object') return isEqualObject(value ,other);
 
   return false
 }
